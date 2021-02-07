220f49fba38a1b5bc36251ad0239035de7ce84cf
=Alisha Soedamah <=Alisha.Soedamah@student.rocva.nl>
2021-02-07 15:46:44 +0100

    1 + 2 from JS

--- 02-Javascript/taak01-Herhaling/js/script.js
+++ 02-Javascript/taak01-Herhaling/js/script.js
@@ -1,2 +1,32 @@
 let strNaam;
-let boolNaamKlopt;
+let boolNaamCorrect;
+
+
+// vraag de gebruiker om zijn naam én sla de opgegeven naam op in strNaam
+strNaam = prompt("Hoe heet je?");
+
+// toon een-confirm box (gebruiker kan op OK of Cancel drukken). Als de gebruiker op OK drukt, wordt er TRUE opgeslagen in de boolNaamCorrect variabele. Als Cancel dan FALSE.
+boolNaamCorrect = confirm("Heet je echt " + strNaam + "?");
+
+//True?
+if (boolNaamCorrect == true) {
+    alert("welkom " + strNaam);
+}
+
+//False?
+else {
+    strNaam = prompt("Nja nog een keer dan: hoe heet je?");
+    boolNaamCorrect = confirm("Heet je echt " + strNaam + "?");
+}
+
+//True this time?
+if (boolNaamCorrect == true) {
+    alert("hiya " + strNaam);
+}
+
+//Still false?
+else {
+    alert("Dat is ook prima, welkom.")
+}
+
+
